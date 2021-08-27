08d86113eb93b2b9cab11ea70fa8d73c7f4e4b00
César Rangel Fonseca da Costa <cesarrangelfonseca@gmail.com>
2021-08-26 22:02:20 -0300

    Validation of API data com celebrate/joi

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
 
@@ -7,12 +8,51 @@ const productController = new ProductController();
 
 productsRouter.get('/', productController.index);
 
-productsRouter.get('/:id', productController.show);
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.show,
+);
 
-productsRouter.post('/', productController.create);
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+);
 
-productsRouter.put('/:id', productController.update);
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.update,
+);
 
-productsRouter.delete('/:id', productController.delete);
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
 
 export default productsRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
+import { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
-import { NextFunction, Request, Response } from 'express-serve-static-core';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 
@@ -16,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((error: Error, request: Request, response: Response, _next: NextFunction) => {
   if (error instanceof AppError) {
